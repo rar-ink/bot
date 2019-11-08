b0db63d2e437934c1e2eadc55cda50ceac5ff49e
rar-ink <veit452@gmail.com>
2019-11-09 01:21:34 +0700

    Update Hook.php

--- Hook.php
+++ Hook.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://api.telegram.org/bot"1004092197:AAE2TX6Fx6_3WOLBG-9ILRTWELtNjStPylQ"/setWebhook?url=https://rpgtestbot.herokuapp.com/index.php";
+$url = "https://api.telegram.org/bot1004092197:AAE2TX6Fx6_3WOLBG-9ILRTWELtNjStPylQ/setWebhook?url=https://rpgtestbot.herokuapp.com/index.php";
 $l1=file_get_contents($url);
 $response = json_decode($l1);
 $s=print_r($response);
