a7aa9eb0a475769f5eeb32d5ce1997ee1a1e2ff0
rar-ink <veit452@gmail.com>
2019-11-09 01:09:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php
+$json = file_get_contents('php://input');
+$obj = json_decode($json, TRUE);
 
-const TOKEN = "1004092197:AAE2TX6Fx6_3WOLBG-9ILRTWELtNjStPylQ";
-const BASE_URL = "https://api.telegram.org/bot" . TOKEN . '/getMe';
-
-$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-
-function sendRequest($method, $params = [])
+if (isset($obj['message']))
 {
-  if(!empty($params)) {
-    $url = BASE_URL . $method . '?' . http_build_query($params);
-  } else {
-    $url = BASE_URL . $method;
-  }
-  return json_decode(
-    file_get_contents($url),
-    JSON_OBJECT_AS_ARRAY
-  );
-}
-
-$time = date('H:m:s');
-
-$chat_id = $update['message']['chat']['id'];
-sendRequest('sendMessage', ['chat_id' => chat_id, 'text' => $time]);
-?>
+$first_name="";
+$last_name="";
+$username="";
+$text="";
+ 
+$chat_id=$obj['message']['chat']['id'];
+if ($obj['message']['chat']['type']=="private")
+{
+if (isset($obj['message']['chat']['first_name'])) $first_name=$obj['message']['chat']['first_name'];
+if (isset($obj['message']['chat']['last_name'])) $last_name=$obj['message']['chat']['last_name'];
+if (isset($obj['message']['chat']['username'])) $username=$obj['message']['chat']['username'];
+ 
+if ($first_name!="" AND $last_name!="") $text="Здравствуйте, ".$first_name." ".$last_name."!";
+if ($text!="")
+{
+$text=urlencode ($text);
+$url = "https://api.telegram.org/bot".$Telegram_botkey."/sendMessage?chat_id=".$chat_id."&amp;text=".$text."&amp;parse_mode=HTML&amp;disable_web_page_preview=false&amp;disable_notification=false";
+$json=file_get_contents($url);
+};
+} //private chat
+}; //message
