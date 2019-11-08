b4766cb9dd670c8d6c075263acf7a1fb5fa21591
rar-ink <veit452@gmail.com>
2019-11-08 21:07:25 +0700

    Add files via upload

---
+++ Hook.php
@@ -0,0 +1,13 @@
+<?php
+
+const TOKEN = "1004092197:AAE2TX6Fx6_3WOLBG-9ILRTWELtNjStPylQ";
+$method = "setWebhook";
+
+$url = "https://api.telegram.org/bot" . TOKEN . "/" . $method;
+$option = [
+    "url" => "https://rpgtestbot.herokuapp.com/index.php"
+]
+
+$responce = file_get_contents($url . "?" . http_build_query($options));
+
+var_dump($responce);
